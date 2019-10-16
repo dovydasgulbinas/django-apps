bb182d0a32f36faee61b46d08ac4a15c7970afed
Dovydas Gulbinas <dovydasgulbinas@gmail.com>
2019-10-16 12:56:05 +0200

    TEST:  Adding before_deploy

--- deploy.sh
+++ deploy.sh
@@ -3,23 +3,12 @@
 pwd
 ls -alt .
 
-source .env
-
-eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 keychain/travis_id_rsa # Allow read access to the private key
-ssh-add keychain/travis_id_rsa # Add the private key to SSH
+# development related env variables only
+source keychain/deploy.env
 
 git config --global push.default matching
 git remote add deploy ssh://$DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PORT$DEPLOY_DIR
 git push deploy "${1:-master}"
-
-
-if [ -z "$encrypted_a1430f60b6d9_iv" ]
-then
-      echo '$encrypted_a1430f60b6d9_iv is empty'
-else
-      echo '$encrypted_a1430f60b6d9_iv is NOT empty'
-fi
 
 ssh $DEPLOY_USER@$DEPLOY_HOST -p $DEPLOY_PORT <<EOF
   cd $DEPLOY_DIR
