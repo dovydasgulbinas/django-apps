c7a289706770178fb46ffe2988a695cb59bb9abf
Dovydas Gulbinas <dovydasgulbinas@gmail.com>
2019-10-17 19:05:44 +0200

    fixing deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-echo $TEST_VAR
+echo "$TEST_VAR"
 
 git config --global push.default matching
 git remote add deploy ssh://$DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PORT$DEPLOY_DIR
 git push deploy "${1:-master}"
 
+# https://blog.fortrabbit.com/deploying-code-with-rsync
+# rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/keychain $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PORT$DEPLOY_DIR
+
+rsync -arvz -e \'"ssh -p $DEPLOY_PORT"\' --progress --delete-after $TRAVIS_BUILD_DIR/.env $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_DIR
+rsync -arvz -e \'"ssh -p $DEPLOY_PORT"\' --progress --delete-after $TRAVIS_BUILD_DIR/keychain $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_DIR
+
+
 ssh $DEPLOY_USER@$DEPLOY_HOST -p $DEPLOY_PORT <<EOF
   cd $DEPLOY_DIR
-  openssl aes-256-cbc -K $encrypted_a1430f60b6d9_key -iv $encrypted_a1430f60b6d9_iv -in secrets.tar.enc -out secrets.tar -d
-  tar xvf secrets.tar  
   make prod-run-new
 EOF
